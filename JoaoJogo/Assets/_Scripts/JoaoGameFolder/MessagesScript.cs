211ee5b0e3ef4a95c708d02cf64cb6fb00815e61
MosquitoEstudante234 <mosquitoestudante34234@gmail.com>
2024-10-17 14:23:52 -0300

    Sistema de mensagem aprimorado

--- JoaoJogo/Assets/_Scripts/JoaoGameFolder/MessagesScript.cs
+++ JoaoJogo/Assets/_Scripts/JoaoGameFolder/MessagesScript.cs
@@ -21,14 +21,24 @@ public class MessagesScript : MonoBehaviour
         // Configura o botão para chamar o método ShowNextObjects ao ser clicado
         nextButton.onClick.AddListener(ShowNextObjects);
 
-        // Mostra apenas o primeiro objeto da lista 1 no início
-        ShowObjectAtIndex(objectsToCycle1, currentIndex1);
+        // Desativa todos os objetos nas listas
+        foreach (GameObject obj in objectsToCycle1)
+        {
+            obj.SetActive(false);
+        }
+        foreach (GameObject obj in objectsToCycle2)
+        {
+            obj.SetActive(false);
+        }
     }
 
     // Método para exibir o próximo objeto das listas
     void ShowNextObjects()
     {
-        // Mostra o próximo objeto da primeira lista
+        // Desativa o botão enquanto processa a exibição dos objetos
+        nextButton.interactable = false;
+
+        // Ativa o próximo objeto da primeira lista
         ShowObjectAtIndex(objectsToCycle1, currentIndex1);
 
         // Avança para o próximo índice da primeira lista
@@ -52,6 +62,11 @@ public class MessagesScript : MonoBehaviour
     IEnumerator ShowNextObjectWithDelay(List<GameObject> objects, int index, float delay)
     {
         yield return new WaitForSeconds(delay);
+
+        // Ativa o próximo objeto da segunda lista
         ShowObjectAtIndex(objects, index);
+
+        // Reativa o botão após o delay
+        nextButton.interactable = true;
     }
 }
