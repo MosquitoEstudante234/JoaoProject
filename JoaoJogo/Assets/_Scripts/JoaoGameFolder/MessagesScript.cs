17adea824e0947f33c9c185eda7a9bd6173c978d
MosquitoEstudante234 <mosquitoestudante34234@gmail.com>
2024-10-17 13:55:58 -0300

    Sistema de mensagem

---
+++ JoaoJogo/Assets/_Scripts/JoaoGameFolder/MessagesScript.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MessagesScript : MonoBehaviour
+{
+    // Lista de GameObjects que pode ser preenchida pelo Inspector
+    public List<GameObject> objectsToCycle1;
+    public List<GameObject> objectsToCycle2;
+
+    // Botão que será clicado para alternar entre os objetos
+    public Button nextButton;
+
+    // Índices atuais dos objetos a serem exibidos
+    private int currentIndex1 = 0;
+    private int currentIndex2 = 0;
+
+    void Start()
+    {
+        // Configura o botão para chamar o método ShowNextObjects ao ser clicado
+        nextButton.onClick.AddListener(ShowNextObjects);
+
+        // Mostra apenas o primeiro objeto da lista 1 no início
+        ShowObjectAtIndex(objectsToCycle1, currentIndex1);
+    }
+
+    // Método para exibir o próximo objeto das listas
+    void ShowNextObjects()
+    {
+        // Mostra o próximo objeto da primeira lista
+        ShowObjectAtIndex(objectsToCycle1, currentIndex1);
+
+        // Avança para o próximo índice da primeira lista
+        currentIndex1 = (currentIndex1 + 1) % objectsToCycle1.Count;
+
+        // Inicia a corrotina para mostrar o próximo objeto da segunda lista após 1 segundo
+        StartCoroutine(ShowNextObjectWithDelay(objectsToCycle2, currentIndex2, 1f));
+
+        // Avança para o próximo índice da segunda lista
+        currentIndex2 = (currentIndex2 + 1) % objectsToCycle2.Count;
+    }
+
+    // Método para mostrar o objeto com o índice fornecido
+    void ShowObjectAtIndex(List<GameObject> objects, int index)
+    {
+        // Ativa o objeto atual
+        objects[index].SetActive(true);
+    }
+
+    // Corrotina para mostrar o próximo objeto da lista após um atraso
+    IEnumerator ShowNextObjectWithDelay(List<GameObject> objects, int index, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        ShowObjectAtIndex(objects, index);
+    }
+}
